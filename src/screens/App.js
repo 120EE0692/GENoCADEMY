73ba3d75a1dfe1f465f99e51d2dd08a445720cc6
Abhas-15 <abhirup.abhas@gmail.com>
2021-12-24 14:02:14 +0530

    feat:chore add playgroun routes

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,14 +1,18 @@
 import LogIn from "../components/LogIn";
 import SignUp from "../components/SignUp";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Playground from "./Play";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<SignUp />}> </Route>
+          <Route exact path="/" element={<SignUp />}>
+            {" "}
+          </Route>
           <Route exact path="/login" element={<LogIn />}></Route>
+          <Route exact path="/playground" element={<Playground />}></Route>
         </Routes>
       </Router>
     </>
