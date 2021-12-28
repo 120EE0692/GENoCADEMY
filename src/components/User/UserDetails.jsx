ad3e4637c7ee03a3b7df3910bf7ea6f0c184f082
Ashish Pradhan <ashishpradhan2345@gmail.com>
2021-12-28 15:39:50 +0530

    feat: fetch user details

--- src/components/User/UserDetails.jsx
+++ src/components/User/UserDetails.jsx
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../config/firebase";
+import { auth, store } from "../../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const UserDetails = () => {
   const [uid, setUid] = useState("");
+  const [userDetails, setUserDetails] = useState({});
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
       const uid = user.uid;
       setUid(uid);
+      // GetUserInfo();
     } else {
-        
     }
   });
 
-  return <>{uid}</>;
+  useEffect(() => GetUserInfo(uid), [uid]);
+
+  const GetUserInfo = async (uid) => {
+    // let docRef = doc(store, "mentorDetails", uid);
+   let docRef = doc(store, "studentDetails", uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+      console.log("Document data:", data);
+      setUserDetails(data.firstName);
+    } else {
+      // doc.data() will be undefined in this case
+      // console.log("No such document!");
+    }
+  };
+
+  return (
+    <>
+      {uid}
+    </>
+  );
 };
 
 export default UserDetails;
