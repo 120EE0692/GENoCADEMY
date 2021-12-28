48c76b0f5c5dff79a1a7fb709fd66eea1cc7e391
swatishree1508 <swatishree1508@gmail.com>
2021-12-28 11:11:12 +0530

    feat:added navbar

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,16 +1,20 @@
 import LogIn from "../components/LogIn";
 import SignUp from "../components/SignUp";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Navbar from "../components/Navbar"
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<SignUp />}> </Route>
-          <Route exact path="/login" element={<LogIn />}></Route>
-        </Routes>
-      </Router>
+    
+        <Router>
+        <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<SignUp />}> </Route>
+            <Route exact path="/login" element={<LogIn />}></Route>
+          </Routes>
+        </Router>
+      
     </>
   );
 }
