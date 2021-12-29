53692a4d1e1760da063e1b4faab47c1406d854f8
Ashish Pradhan <ashishpradhan2345@gmail.com>
2021-12-29 14:27:25 +0530

    feat: get user detail using id

--- src/components/User/UserDetails.jsx
+++ src/components/User/UserDetails.jsx
@@ -1,40 +1,35 @@
-import React, { useState, useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth, store } from "../../config/firebase";
+import React, { useState, useEffect, useContext } from "react";
+import { store } from "../../config/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import authContext from "../../context/AuthContext";
 
 const UserDetails = () => {
-  const [uid, setUid] = useState("");
-  const [userDetails, setUserDetails] = useState({});
+  const id = useContext(authContext);
+  const [userDetails, setUserDetails] = useState([]);
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      setUid(uid);
-      // GetUserInfo();
-    } else {
-    }
-  });
-
-  useEffect(() => GetUserInfo(uid), [uid]);
-
-  const GetUserInfo = async (uid) => {
-    // let docRef = doc(store, "mentorDetails", uid);
-   let docRef = doc(store, "studentDetails", uid);
+  useEffect(() => GetUserInfo(id), []);
+  const GetUserInfo = async (id) => {
+    const docRef = doc(store, "userDetails", id);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       const data = docSnap.data();
       console.log("Document data:", data);
-      setUserDetails(data.firstName);
+      setUserDetails(Object.entries(data));
     } else {
       // doc.data() will be undefined in this case
       // console.log("No such document!");
     }
   };
-
   return (
     <>
-      {uid}
+      {id}
+      <div>
+        {userDetails?.map((user) => (
+          <div>
+            {user[0].toUpperCase(0)} : {user[1]}
+          </div>
+        ))}
+      </div>
     </>
   );
 };
