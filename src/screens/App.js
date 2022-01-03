dd0c152275d2806c088777564230dbe53730aca1
Ashish Pradhan <ashishpradhan2345@gmail.com>
2022-01-03 12:28:31 +0530

    feat: add watch route

--- src/screens/App.js
+++ src/screens/App.js
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 
 import LogIn from "../components/User/LogIn";
 import SignUp from "../components/User/SignUp";
+import Player from "./Player";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Playground from "./Play";
 import AuthState from "../context/AuthState";
@@ -18,6 +19,7 @@ function App() {
             </Route>
             <Route exact path="/login" element={<LogIn />}></Route>
             <Route exact path="/playground" element={<Playground />}></Route>
+            <Route exact path="/watch" element={<Player />}></Route>
           </Routes>
         </Router>
       </AuthState>
