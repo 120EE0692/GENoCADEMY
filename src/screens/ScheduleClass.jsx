644ba81a1d8efc20e47ff1e2865b1fcbd67be337
Ashish Pradhan <ashishpradhan2345@gmail.com>
2022-01-04 11:23:34 +0530

    feat: add scheduleClass screen

---
+++ src/screens/ScheduleClass.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CreateLiveClassForm from "../components/CreateLiveClassForm";
+
+const ScheduleClass = () => {
+  return <CreateLiveClassForm />;
+};
+
+export default ScheduleClass;
