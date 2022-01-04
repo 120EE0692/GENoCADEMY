6ead67d531994263b12bb594c877f2965afe9e45
Ashish Pradhan <ashishpradhan2345@gmail.com>
2022-01-04 11:23:56 +0530

    feat: add route

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,11 +1,17 @@
-import Navbar from "../components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+//components
+import Navbar from "../components/Navbar";
 import LogIn from "../components/User/LogIn";
 import SignUp from "../components/User/SignUp";
 import Player from "./Player";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AllClass from "./AllClass";
+import ScheduleClass from "./ScheduleClass";
 import Playground from "./Play";
+
+//Context
 import AuthState from "../context/AuthState";
+
 
 function App() {
   return (
@@ -20,6 +26,8 @@ function App() {
             <Route exact path="/login" element={<LogIn />}></Route>
             <Route exact path="/playground" element={<Playground />}></Route>
             <Route exact path="/watch" element={<Player />}></Route>
+            <Route exact path="/class" element={<AllClass />}></Route>
+            <Route exact path="/scheduleclass" element={<ScheduleClass />} ></Route>
           </Routes>
         </Router>
       </AuthState>
