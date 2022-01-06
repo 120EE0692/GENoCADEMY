8e42997af03f31424b063aed61278bbe15b3bccc
Ashish Pradhan <ashishpradhan2345@gmail.com>
2022-01-06 14:04:28 +0530

    feat: add dashboard file

---
+++ src/screens/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import authContext from "../context/AuthContext";
+
+const Dashboard = () => {
+  const logInUserInfo = useContext(authContext);
+  console.log(logInUserInfo);
+  return <>Hi {logInUserInfo.loginUserName.toUpperCase()}</>;
+};
+
+export default Dashboard;
