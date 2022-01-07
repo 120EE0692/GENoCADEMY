8ff65b57f4feb4427670e32616f5b59dd5a5cac9
Ashish Pradhan <ashishpradhan2345@gmail.com>
2022-01-07 12:57:48 +0530

    fix: add optional chaining

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -4,7 +4,7 @@ import authContext from "../context/AuthContext";
 const Dashboard = () => {
   const logInUserInfo = useContext(authContext);
   console.log(logInUserInfo);
-  return <>Hi {logInUserInfo.loginUserName.toUpperCase()}</>;
+  return <>Hi {logInUserInfo.loginUserName?.toUpperCase()}</>;
 };
 
 export default Dashboard;
