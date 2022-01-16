2c17517c30a66901181a3407c7d065d89809d59e
Ashish Pradhan <ashishpradhan2345@gmail.com>
2022-01-16 16:32:32 +0530

    feat: add firestore in checklist

--- src/components/checklist/ToDo/App.js
+++ src/components/checklist/ToDo/App.js
@@ -1,43 +1,96 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+
+//components
 import ToDoList from "./ToDoList";
-import data from "./data.json";
 import Check from "../Check";
+
 //mui
 import { makeStyles } from "@mui/styles";
 import { Typography } from "@mui/material";
 
+//context
+import authContext from "../../../context/AuthContext";
+
+//firebase
+import { store } from "../../../config/firebase";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+
 const App = () => {
   const classes = useStyle();
-  const [ toDoList, setToDoList ] = useState(data);
+
+  const [toDoList, setToDoList] = useState([]);
+  const [length, setLength] = useState(0);
+
+  const { id } = useContext(authContext);
+
   const handleToggle = (id) => {
-    let mapped = toDoList.map(task => {
-      return task.id == id ? { ...task, complete: !task.complete } : { ...task};
+    let mapped = toDoList?.map((task) => {
+      return task.id == id
+        ? { ...task, complete: !task.complete }
+        : { ...task };
     });
     setToDoList(mapped);
-  }
+  };
+
   const handleFilter = () => {
-    let filtered = toDoList.filter(task => {
+    let filtered = toDoList?.filter((task) => {
       return !task.complete;
     });
     setToDoList(filtered);
-  }
-  const addTask = (userInput ) => {
+    setDoc(doc(store, "checklist", id), { ...filtered });
+  };
+
+  const addTask = async (userInput) => {
     let copy = [...toDoList];
-    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    copy = [
+      ...copy,
+      { id: toDoList.length + 1, task: userInput, complete: false },
+    ];
     setToDoList(copy);
-  }
+  };
+
+  useEffect(async () => {
+    if (toDoList.length === 0) {
+      while (toDoList.length === 0) {
+        const dblist = await getDoc(doc(store, "checklist", id));
+        if (dblist) {
+          setLength(Object.keys(dblist.data()).length);
+          setToDoList([...toDoList, ...Object.values(dblist.data())]);
+          break;
+        } else {
+          setDoc(doc(store, "checklist", id), {});
+        }
+
+        if (toDoList.length) break;
+      }
+    }
+  }, [id]);
+
+  useEffect(async () => {
+    if (toDoList.length > length) {
+      try {
+        if (id) await setDoc(doc(store, "checklist", id), { ...toDoList });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [toDoList]);
 
   return (
     <>
       <div className={classes.container}>
         <div className={classes.Wrapper}>
           <div>
-            <Check addTask={addTask}/>
+            <Check addTask={addTask} />
             <div className={classes.heading}>
               <Typography component="h2" variant="h6">
-                Here's your To-Do List. <br/> Happy Learning! 
+                Here's your To-Do List. <br /> Happy Learning!
               </Typography>
-              <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+              <ToDoList
+                toDoList={toDoList}
+                handleToggle={handleToggle}
+                handleFilter={handleFilter}
+              />
             </div>
           </div>
         </div>
@@ -59,12 +112,11 @@ const useStyle = makeStyles((theme) => ({
     alignItems: "center",
     textAlign: "center",
   },
-    Wrapper: {
+  Wrapper: {
     width: "50%",
     backgroundColor: "#EBFFFB",
     [theme.breakpoints.down("md")]: {
       width: "100%",
     },
   },
-  
 }));
