f61d1b555e4b73954783e57023b76d13f040d40c
Abhas-15 <abhirup.abhas@gmail.com>
2022-01-18 23:48:40 +0530

    feat:create discussion forum

---
+++ src/components/Discussions/DiscussionForumPage.jsx
@@ -0,0 +1,11 @@
+import Comments from "./Discussions";
+
+const DiscussionForumPage = () => {
+  return (
+    <div>
+      <Comments currentUserId="1" />
+    </div>
+  );
+};
+
+export default DiscussionForumPage;
