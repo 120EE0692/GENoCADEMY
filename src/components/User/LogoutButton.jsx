d55cdb5adc8beb13435fe499f183eb2d181c604a
Ashish Pradhan <ashishpradhan2345@gmail.com>
2022-01-19 18:50:21 +0530

    feat: navigate after logout

--- src/components/User/LogoutButton.jsx
+++ src/components/User/LogoutButton.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import { Button } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
@@ -7,9 +8,12 @@ import { auth } from "../../config/firebase";
 import { signOut } from "firebase/auth";
 
 const LogoutButton = () => {
+  const navigate = useNavigate();
   const logout = async () => {
     try {
       await signOut(auth);
+      localStorage.clear()
+      navigate("/login")
     } catch (error) {
       console.log(error);
     }
