cd9df6f1d394a4b049669a2497e451ecd4bf049e
Ashish Pradhan <ashishpradhan2345@gmail.com>
2022-01-19 18:50:45 +0530

    style: change dashboard styles

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -5,16 +5,14 @@ import Typography from "@mui/material/Typography";
 
 const Dashboard = () => {
   const logInUserInfo = useContext(authContext);
-  console.log(logInUserInfo);
   return (
     <>
-      {" "}
-      <Typography style={{ color: "#006064" }}>
-        {" "}
-        <h2>
-          Hi{" "}
-          <em className="name">{logInUserInfo.loginUserName?.toUpperCase()}</em>{" "}
-        </h2>
+      <Typography style={{ color: "#006064", fontWeight: "900", fontSize: "4rem" }}>
+        Hi{" "}
+        <em className="name">{logInUserInfo.loginUserName?.toUpperCase()}</em>{" "}
+      </Typography>
+      <Typography style={{ color: "#8FA01F", fontWeight: "700", fontSize: "3rem", textAlign: "center", background: "#DED93E", margin: "2rem 0", padding: "1rem 0" }}>
+        “If You are planning for a year, sow rice; if you are planning for a decade, plant trees; if you are planning for a lifetime, educate people”
       </Typography>
     </>
   );
